62ac81d575d01057ff62aae8a447e7ff7392d83b
Afsal-ap <afsalapkvr@gmail.com>
2025-04-02 10:55:48 +0530

    cors done

--- backend/ad-service/src/index.ts
+++ backend/ad-service/src/index.ts
@@ -16,7 +16,7 @@ const upload = multer({ dest: 'uploads/' });
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'https://frontend-zeta-rose-49.vercel.app',
+    origin: 'https://www.worldtoday.shop',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
